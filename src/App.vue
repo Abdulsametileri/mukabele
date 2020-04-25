8f8ac5f3e4a8ba7aa64616d0626798f19f3e82e7
A.Samet İleri <sametileri07@gmail.com>
2020-04-25 21:18:27 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
       <h3>Geçmiş Yayınlarımız</h3>
 
       <a href="https://www.youtube.com/watch?v=oB1zixMQEro">RAMAZAN MUKABELESİ 1. CÜZ</a>
+      <br>
       <a href="https://www.youtube.com/watch?v=5jKn8cbNa-E">RAMAZAN MUKABELESİ 2. CÜZ</a>
     </h3>
   </div>
