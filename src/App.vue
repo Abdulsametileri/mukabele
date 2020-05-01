84fc381f874da5170f6dce3c284fef590d9e2fcd
A.Samet İleri <sametileri07@gmail.com>
2020-05-01 05:02:08 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,8 @@
       <a href="https://www.youtube.com/watch?v=Vg1BoPqmYVY">RAMAZAN MUKABELESİ 5. CÜZ</a>
       <br>
       <a href="https://www.youtube.com/watch?v=zE9Vveev2PQ">RAMAZAN MUKABELESİ 6. CÜZ</a>
+      <br>
+      <a href="https://www.youtube.com/watch?v=j7oN4K593Zk">RAMAZAN MUKABELESİ 7. CÜZ</a>
     </h3>
   </div>
 </template>
@@ -36,8 +38,8 @@
     data() {
       return {
         katilimciMi: false,
-        streamyard: 'https://streamyard.com/8gh46kbttp',
-        youtube: 'https://www.youtube.com/watch?v=j7oN4K593Zk'
+        streamyard: 'https://streamyard.com/xyuhnupn6b',
+        youtube: 'https://www.youtube.com/watch?v=DJhUI13_oMc'
       }
     },
     created() {
